8b09acf19994505606c5f9da2fcd00ddebb47a96
Andy Geach <andy@bugorfeature.net>
2021-02-26 13:20:55 +0000

    Better way to do alternative data sources

--- sysproduction/update_historical_prices_barchart.py
+++ sysproduction/update_historical_prices_barchart.py
@@ -5,21 +5,16 @@ Update historical data per contract from Barchart data, dump into mongodb
 from syscore.objects import success, failure
 from syscore.merge_data import spike_in_data
 
-from sysdata.futures.futures_per_contract_prices import DAILY_PRICE_FREQ
-
 from sysobjects.contracts import futuresContract
 
 from sysdata.data_blob import dataBlob
 from sysproduction.data.prices import diagPrices, updatePrices
+from sysproduction.data.alt_data_source import altDataSource
 from sysproduction.data.contracts import diagContracts
 from syslogdiag.email_via_db_interface import send_production_mail_msg
 from syscore.objects import arg_not_supplied
 
 from sysdata.barchart.barchart_futures_contract_price_data import barchartFuturesContractPriceData
-from sysdata.arctic.arctic_futures_per_contract_prices import arcticFuturesContractPriceData, futuresContractPrices
-from sysdata.arctic.arctic_multiple_prices import arcticFuturesMultiplePricesData, futuresMultiplePrices
-from sysdata.arctic.arctic_adjusted_prices import arcticFuturesAdjustedPricesData, futuresAdjustedPrices
-from sysdata.mongodb.mongo_futures_contracts import mongoFuturesContractData
 
 
 def update_historical_prices_barchart():
@@ -29,12 +24,7 @@ def update_historical_prices_barchart():
     :return: Nothing
     """
     with dataBlob(log_name="Update-Barchart-Historical-Prices",
-                  ib_conn=arg_not_supplied,
-                  class_list=[barchartFuturesContractPriceData,
-                              arcticFuturesContractPriceData,
-                              arcticFuturesMultiplePricesData,
-                              arcticFuturesAdjustedPricesData,
-                              mongoFuturesContractData]) as data:
+                  ib_conn=arg_not_supplied) as data:
         update_historical_price_object = updateBarchartHistoricalPrices(data)
         update_historical_price_object.update_historical_prices()
     return success
@@ -109,19 +99,24 @@ def update_historical_prices_for_instrument_and_contract(
 
 
 def get_and_add_prices_for_frequency(
-        data: dataBlob, contract_object: futuresContract, frequency: str="D"):
-    #broker_data_source = dataBroker(data)
+        data: dataBlob, contract_object: futuresContract, frequency: str = "D"):
+
+    read_data = dataBlob(log_name="Source-Historical-Prices-Barchart",
+                         ib_conn=arg_not_supplied,
+                         class_list=[barchartFuturesContractPriceData])
+
+    data_source = altDataSource(read_data)
     db_futures_prices = updatePrices(data)
 
-    barchart_prices = db_futures_prices.get_prices_at_frequency_for_contract_object(
+    prices = data_source.get_prices_at_frequency_for_contract_object(
             contract_object, frequency)
 
-    if len(barchart_prices)==0:
-        data.log.msg("No prices from barchart for %s" % str(contract_object))
+    if len(prices) == 0:
+        data.log.msg("No prices from Barchart for %s" % str(contract_object))
         return failure
 
     error_or_rows_added = db_futures_prices.update_prices_for_contract(
-        contract_object, barchart_prices, check_for_spike=True
+        contract_object, prices, check_for_spike=True
     )
 
     if error_or_rows_added is spike_in_data:
@@ -133,6 +128,7 @@ def get_and_add_prices_for_frequency(
         % (error_or_rows_added, frequency, str(contract_object))
     )
     return success
+
 
 def report_price_spike(data: dataBlob, contract_object: futuresContract):
     # SPIKE
