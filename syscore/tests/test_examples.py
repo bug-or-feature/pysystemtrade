2413e47d2cf166187544d77aa751aebb2407eef7
Andy Geach <andy@bugorfeature.net>
2021-03-03 11:03:46 +0000

    test examples

---
+++ syscore/tests/test_examples.py
@@ -0,0 +1,17 @@
+
+
+class TestExamples:
+
+    def test_simple_trading_rule(self):
+        """
+        This is the 'Simple Trading Rule' code sample given in docs/introduction.md
+        """
+        import examples.introduction.asimpletradingrule
+
+    def test_simple_system(self):
+        """
+        This is the 'Simple System' code sample given in docs/introduction.md
+        """
+        import examples.introduction.simplesystem
+
+
