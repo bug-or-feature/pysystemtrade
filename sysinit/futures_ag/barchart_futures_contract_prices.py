26ee9e977fa86cd5e0df76b016617e1af4f0c748
Andy Geach <andy@bugorfeature.net>
2021-04-02 07:36:31 +0100

    data import scripts

---
+++ sysinit/futures_ag/barchart_futures_contract_prices.py
@@ -0,0 +1,61 @@
+from sysdata.csv.csv_futures_contract_prices import ConfigCsvFuturesPrices
+from sysinit.futures.contract_prices_from_csv_to_arctic import init_arctic_with_csv_futures_contract_prices
+from sysinit.futures.contract_prices_from_csv_to_arctic import init_arctic_with_csv_futures_contract_prices_for_code
+
+market_map = dict(AE='AEX',
+                  A6='AUD',
+                  HR='BOBL',
+                  II='BTP',
+                  MX='CAC',
+                  GG='BUND',
+                  HG='COPPER',
+                  ZC='CORN',
+                  CL='CRUDE_W',
+                  GE='EDOLLAR',
+                  E6='EUR',
+                  NG='GAS_US',
+                  B6='GBP',
+                  GC='GOLD',
+                  J6='JPY',
+                  HE='LEANHOG',
+                  LE='LIVECOW',
+                  M6='MXP',
+                  NQ='NASDAQ',
+                  N6='NZD',
+                  FN='OAT',
+                  PA='PALLAD',
+                  HF='SHATZ',
+                  PL='PLAT',
+                  SZ='SMI',
+                  ZS='SOYBEAN',
+                  ES='SP500',
+                  ZT='US2',
+                  ZF='US5',
+                  ZN='US10',
+                  ZB='US20',
+                  VI='VIX',
+                  ZW='WHEAT',
+                  DV='V2X',
+                  UD='US30')
+
+barchart_csv_config = ConfigCsvFuturesPrices(input_date_index_name="Time",
+                                input_skiprows=0, input_skipfooter=0,
+                                input_date_format='%Y-%m-%dT%M:%H:%S%z',
+                                input_column_mapping=dict(OPEN='Open',
+                                                          HIGH='High',
+                                                          LOW='Low',
+                                                          FINAL='Close',
+                                                          VOLUME='Volume'
+                                                          ))
+
+def transfer_barchart_prices_to_arctic(datapath):
+    init_arctic_with_csv_futures_contract_prices(datapath, csv_config= barchart_csv_config)
+
+def transfer_barchart_prices_to_arctic_single(instr, datapath):
+    init_arctic_with_csv_futures_contract_prices_for_code(instr, datapath, csv_config= barchart_csv_config)
+
+if __name__ == "__main__":
+    input("Will overwrite existing prices are you sure?! CTL-C to abort")
+    datapath = "/Users/ageach/Dev/work/pyhistprice/data/barchart_tz_anal"
+    transfer_barchart_prices_to_arctic(datapath)
+    #transfer_barchart_prices_to_arctic_single('GOLD', datapath)
---
+++ sysinit/futures_ag/roll_parameters_csv_mongo.py
@@ -0,0 +1,25 @@
+"""
+Populate a mongo DB collection with roll data from a csv
+"""
+
+from sysdata.mongodb.mongo_roll_data import mongoRollParametersData
+from sysdata.csv.csv_roll_parameters import csvRollParametersData
+
+if __name__ == "__main__":
+    input("Will overwrite existing data are you sure?! CTL-C to abort")
+    # modify flags as required
+
+    data_out = mongoRollParametersData()
+    data_in = csvRollParametersData(datapath="sysinit.futures_ag.config.")
+
+    instrument_list = data_in.get_list_of_instruments()
+
+    for instrument_code in instrument_list:
+        instrument_object = data_in.get_roll_parameters(instrument_code)
+
+        data_out.delete_roll_parameters(instrument_code, are_you_sure=True)
+        data_out.add_roll_parameters(instrument_code, instrument_object)
+
+        # check
+        instrument_added = data_out.get_roll_parameters(instrument_code)
+        print("Added %s: %s to %s" % (instrument_code, instrument_added, data_out))
