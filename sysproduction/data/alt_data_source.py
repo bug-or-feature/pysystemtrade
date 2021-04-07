7db1e8100bcded848613a5374df64855fc3e11d5
Andy Geach <andy@bugorfeature.net>
2021-04-07 09:14:54 +0100

    adding the method to get expiry dates

--- sysproduction/data/alt_data_source.py
+++ sysproduction/data/alt_data_source.py
@@ -2,6 +2,7 @@ from syscore.objects import arg_not_supplied
 from sysdata.data_blob import dataBlob
 from sysobjects.spot_fx_prices import fxPrices
 from sysobjects.contracts import futuresContract
+from sysobjects.contract_dates_and_expiries import expiryDate
 from sysdata.arctic.arctic_futures_per_contract_prices import futuresContractPrices
 
 
@@ -27,6 +28,10 @@ class altDataSource(object):
         return self.data.db_futures_contract_price.get_prices_at_frequency_for_contract_object(
                 contract_object, frequency)
 
+    def get_actual_expiry_date_for_single_contract(
+            self, futures_contract: futuresContract) -> expiryDate:
 
-
+        # TODO check the source has been wired up ok?
+        return self.data.db_futures_contract.get_actual_expiry_date_for_single_contract(
+            futures_contract)
 
