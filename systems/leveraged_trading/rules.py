9143b57d4ae4f85ea914725883bf6b41e4a45290
Andy Geach <andy@bugorfeature.net>
2021-06-24 17:45:12 +0100

    Adding risk adjusted SMAC rule

--- systems/leveraged_trading/rules.py
+++ systems/leveraged_trading/rules.py
@@ -2,6 +2,7 @@
 Trading rules for Leveraged Trading system
 """
 import pandas as pd
+from sysquant.estimators.vol import robust_vol_calc
 
 
 def smac(price, fast=16, slow=64):
@@ -27,3 +28,33 @@ def smac(price, fast=16, slow=64):
     raw_smac = fast_sma - slow_sma
 
     return raw_smac
+
+def rasmac(price, fast=16, slow=64, lookback=25):
+    """
+    Calculate the risk adjusted SMAC (Simple Moving Average Crossover) trading fule forecast, given a price and
+    SMA speeds fast, slow, and volatility lookback
+
+    Assumes that 'price' is daily data
+
+    :param price: The price or other series to use (assumed Tx1)
+    :type price: pd.Series
+
+    :param fast: Lookback for fast in days
+    :type fast: int
+
+    :param slow: Lookback for slow in days
+    :type slow: int
+
+    :param volatility_lookback_days
+    :type volatility_lookback_days: int
+
+    :returns: pd.Series unscaled, uncapped forecast
+    """
+
+    fast_sma = price.ffill().rolling(window=fast).mean()
+    slow_sma = price.ffill().rolling(window=slow).mean()
+    raw_smac = fast_sma - slow_sma
+
+    vol = robust_vol_calc(price, lookback)
+
+    return raw_smac / vol.ffill()
