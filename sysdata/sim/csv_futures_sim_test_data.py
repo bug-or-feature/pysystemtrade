3bde9d269ac8f0ebdabe301ab35da03c52f22e0c
Andy Geach <andy@bugorfeature.net>
2021-09-27 12:17:48 +0100

    calculate adjusted prices on the fly

--- sysdata/sim/csv_futures_sim_test_data.py
+++ sysdata/sim/csv_futures_sim_test_data.py
@@ -2,7 +2,6 @@ import pandas as pd
 from datetime import datetime
 
 from sysdata.csv.csv_multiple_prices import csvFuturesMultiplePricesData
-from sysdata.csv.csv_adjusted_prices import csvFuturesAdjustedPricesData
 from sysdata.csv.csv_spot_fx import csvFxPricesData
 from sysdata.csv.csv_instrument_data import csvFuturesInstrumentData
 from sysdata.csv.csv_roll_parameters import csvRollParametersData
@@ -21,7 +20,8 @@ class CsvFuturesSimTestData(genericBlobUsingFuturesSimData):
     """
     Specialisation of futuresSimData that allows start and end dates to be configured.
     Useful for unit tests, so that new data added to the CSV price files doesn't mess with assertions,
-    or if a test is needed at a certain date/time or period
+    or if a test is needed at a certain date/time or period.
+    Adjusted prices are calculated on the fly
     """
 
     DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
@@ -33,9 +33,7 @@ class CsvFuturesSimTestData(genericBlobUsingFuturesSimData):
 
         data = dataBlob(
             log=log,
-            csv_data_paths=dict(csvFuturesAdjustedPricesData="data.test.adjusted_prices_csv"),
             class_list=[
-                csvFuturesAdjustedPricesData,
                 csvFuturesMultiplePricesData,
                 csvFuturesInstrumentData,
                 csvFxPricesData,
@@ -55,6 +53,8 @@ class CsvFuturesSimTestData(genericBlobUsingFuturesSimData):
         else:
             self._end_date = self.DEFAULT_END_DATE
 
+        self._adjusted_prices = dict()
+
     def __repr__(self):
         return f"csvFuturesSimTestData with {self.get_instrument_list()} instruments, " \
                f"start date {self.start_date.strftime(self.DATE_FORMAT)}, " \
@@ -69,9 +69,12 @@ class CsvFuturesSimTestData(genericBlobUsingFuturesSimData):
         return self._end_date
 
     def get_backadjusted_futures_price(self, instrument_code: str) -> futuresAdjustedPrices:
-        data = super().db_futures_adjusted_prices_data.get_adjusted_prices(instrument_code)
-        date_adjusted = data[self.start_date:self.end_date]
-        return date_adjusted
+        if instrument_code in self._adjusted_prices:
+            return self._adjusted_prices[instrument_code]
+        else:
+            adj_prices = self._calc_adj_prices(instrument_code)
+            self._adjusted_prices[instrument_code] = adj_prices
+            return adj_prices
 
     def get_multiple_prices(self, instrument_code: str) -> futuresMultiplePrices:
         data = super().get_multiple_prices(instrument_code)
@@ -97,3 +100,9 @@ class CsvFuturesSimTestData(genericBlobUsingFuturesSimData):
         data = super().get_current_and_forward_price_data(instrument_code)
         date_adjusted = data[self.start_date:self.end_date]
         return date_adjusted
+
+    def _calc_adj_prices(self, instrument_code):
+        data = super().get_multiple_prices(instrument_code)
+        date_adjusted = data[self.start_date:self.end_date]
+        adjusted_prices = futuresAdjustedPrices.stitch_multiple_prices(date_adjusted)
+        return adjusted_prices
