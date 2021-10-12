084ce1007ecc2f2ef588eb5b64e186d3dbaf0c5e
Andy Geach <andy@bugorfeature.net>
2021-10-12 18:08:29 +0100

    update list updated with new instruments

--- sysproduction/update_all.py
+++ sysproduction/update_all.py
@@ -2,27 +2,20 @@ from sysproduction.update_fx_prices import update_fx_prices
 from sysproduction.update_historical_prices import update_historical_prices
 from sysproduction.update_multiple_adjusted_prices import update_multiple_adjusted_prices
 from sysproduction.update_sampled_contracts import update_sampled_contracts
+from sysproduction.data.prices import diagPrices
 
 
 def update_all():
 
-    # full_instr_list = ['AEX', 'ASX', 'AUD', 'BOBL', 'BRENT_W', 'BTP', 'BUND', 'BUXL', 'CAC', 'CARBON',
-    #               'CHF', 'COCOA_LDN', 'COCOA_NY', 'COFFEE', 'COFFEE_LDN', 'COPPER', 'CORN', 'COTTON',
-    #               'CRUDE_W', 'DAX', 'DOLLAR', 'DOW', 'EDOLLAR', 'EUR', 'EURGBP', 'EURIBOR', 'EUROSTX',
-    #               'FTSE', 'GASOIL_LDN', 'GASOLINE', 'GAS_US', 'GBP', 'GILT', 'GOLD', 'HANG', 'HEATOIL',
-    #               'JGB', 'JPY', 'LEANHOG', 'LIVECOW', 'LUMBER', 'MXP', 'NASDAQ', 'NIKKEI', 'NZD', 'OAT',
-    #               'OATIES', 'OJ', 'PALLAD', 'PLAT', 'RICE', 'RUSSELL', 'SHATZ', 'SILVER', 'SMI', 'SOYBEAN',
-    #               'SOYMEAL', 'SOYOIL', 'SP500', 'STERLING3', 'SUGAR', 'SUGAR_LDN', 'US10', 'US2', 'US30',
-    #               'US5', 'USTB', 'V2X', 'VIX', 'WHEAT', 'WHEAT_LDN']
+    instr_list = ["AEX", "ASX", "AUD", "BOBL", "BRENT_W", "BTP", "BUND", "BUXL", "CAC", "CAD", "CHF", "COCOA_NY",
+                  "COFFEE", "COPPER", "CORN", "COTTON", "CRUDE_W", "DAX", "DOLLAR", "DOW", "EDOLLAR", "EUR", "EURGBP",
+                  "EURIBOR", "EUROSTX", "FTSE", "GASOLINE", "GAS_US", "GBP", "GILT", "GOLD", "HANG", "HEATOIL", "JGB",
+                  "JPY", "LEANHOG", "LIVECOW", "LUMBER", "NASDAQ", "NIKKEI", "NZD", "OAT", "OATIES", "OJ", "PALLAD",
+                  "PLAT", "RUSSELL", "SHATZ", "SMI", "SOYBEAN", "SP500", "STERLING3", "US10", "US2", "US30", "US5",
+                  "USTB", "V2X", "VIX", "WHEAT"]
 
-    instr_list = ['AEX', 'ASX', 'AUD', 'BOBL', 'BTP', 'BUND', 'BUXL', 'CAC',
-                  'CHF', 'COPPER', 'CORN',
-                  'CRUDE_W', 'DAX', 'DOLLAR', 'DOW', 'EDOLLAR', 'EUR', 'EURGBP', 'EUROSTX',
-                  'FTSE', 'GAS_US', 'GBP', 'GILT', 'GOLD', 'HANG',
-                  'JGB', 'JPY', 'LEANHOG', 'LIVECOW', 'NASDAQ', 'NIKKEI', 'NZD', 'OAT',
-                  'PALLAD', 'PLAT', 'RUSSELL', 'SHATZ', 'SMI', 'SOYBEAN',
-                  'SP500', 'US10', 'US2', 'US30',
-                  'US5', 'USTB', 'V2X', 'VIX', 'WHEAT']
+    # prices = diagPrices()
+    # print(prices.get_list_of_instruments_in_multiple_prices())
 
     update_fx_prices()
     update_sampled_contracts(instrument_list=instr_list)
