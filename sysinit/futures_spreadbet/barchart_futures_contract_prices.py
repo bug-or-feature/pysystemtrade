896b407fc62f2c2808216cd2b69e34e3ce4b2818
Andy Geach <andy@bugorfeature.net>
2021-10-13 14:35:23 +0100

    tweaks for manual price adjustments

--- sysinit/futures_spreadbet/barchart_futures_contract_prices.py
+++ sysinit/futures_spreadbet/barchart_futures_contract_prices.py
@@ -111,11 +111,17 @@ if __name__ == "__main__":
     #for contract_date in ['DAX.20201200', 'DAX.20210300', 'DAX.20210600', 'DAX.20210900', 'DAX.20211200']:
     #for contract_date in ['20200100', '20200200', '20200300', '20200400','20200500', '20200600', '20200700', '20200800','20200900', '20201000', '20201100', '20201200','20210100', '20210200', '20210300', '20210400','20210500', '20210600', '20210700', '20210800','20210900', '20211000', '20211100', '20211200']:
     #for contract in ['DAX.20210600', 'DAX.20210900', 'DAX.20211200']:
-    instr = "LUMBER"
+    instr = "SUGAR_LDN"
+    #years = ["2020","2021","2022"]
+    #months = ["01","03","05","07","08","09","10","12"]
     #for contract_date in ['20200900','20201200','20210300','20210500','20210700','20210900','20211200','20220300','20220500','20220700','20220900','20221200']:
-    #for contract_date in ['20210300', '20210500', '20210700', '20210900', '20211200', '20220300']:
-    for contract_date in ['20150100']:
+    #for contract_date in ['20210300', '20210500', '20210700', '20210900', '20211100',   '20210300', '20210500', '20210700', '20210900', '20211100',]:
+    for contract_date in ['19950800']:
         transfer_barchart_prices_to_arctic_single_contract(instr, contract_date, datapath)
+    # for year in years:
+    #     for month in months:
+    #         contract_date = f"{year}{month}00"
+    #         print(f"code: {contract_date}")
 
     #for contract_date in ['20080800']:
     # transfer_barchart_prices_to_arctic_single_contract(instr, contract_date, datapath)
