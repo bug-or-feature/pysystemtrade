7eea0c4daa2bb7e6f097039b31e512b68767c950
Andy Geach <andy@bugorfeature.net>
2021-10-19 14:11:12 +0100

    display full dataframes in solo roll report

--- sysproduction/run_solo_reports.py
+++ sysproduction/run_solo_reports.py
@@ -1,15 +1,16 @@
 from sysdata.data_blob import dataBlob
-from sysproduction.reporting.reporting_functions import run_report_with_data_blob
+from sysproduction.reporting.reporting_functions import run_report_with_data_blob, pandas_display_for_reports
 from sysproduction.reporting.roll_report import ALL_ROLL_INSTRUMENTS
 from sysproduction.reporting.report_configs import reportConfig
 
-def run_solo_roll_report():
+def run_solo_roll_report(format="console"):
     config = reportConfig(
         title="Roll report",
         function="sysproduction.reporting.roll_report.roll_info",
         instrument_code=ALL_ROLL_INSTRUMENTS,
-        output="email"
+        output=format
     )
+    pandas_display_for_reports()
     with dataBlob(log_name="Email-Roll-Report") as data:
         run_report_with_data_blob(config, data)
 
