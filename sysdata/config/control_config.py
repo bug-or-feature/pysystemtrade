c9d98770e43f0d88a8931ca98cb9bce25cfc33cb
Andy Geach <andy@bugorfeature.net>
2021-10-29 10:25:39 +0100

    better error handling when reading control config

--- sysdata/config/control_config.py
+++ sysdata/config/control_config.py
@@ -1,4 +1,5 @@
 from sysdata.config.configdata import Config
+from yaml.parser import ParserError
 
 PRIVATE_CONTROL_CONFIG_FILE = "private.private_control_config.yaml"
 DEFAULT_CONTROL_CONFIG_FILE = "syscontrol.control_config.yaml"
@@ -7,8 +8,12 @@ def get_control_config() -> Config:
     try:
         control_config = Config(PRIVATE_CONTROL_CONFIG_FILE, default_filename=DEFAULT_CONTROL_CONFIG_FILE)
         control_config.fill_with_defaults()
-    except:
+    except ParserError as pe:
+        raise Exception("YAML syntax problem: %s" % str(pe))
+    except FileNotFoundError:
         raise Exception("Need to have either %s or %s or both present:" % (
-        str(DEFAULT_CONTROL_CONFIG_FILE), str(PRIVATE_CONTROL_CONFIG_FILE)))
+            str(DEFAULT_CONTROL_CONFIG_FILE), str(PRIVATE_CONTROL_CONFIG_FILE)))
+    except BaseException as be:
+        raise Exception("Problem reading control config: %s" % str(be))
 
     return control_config
