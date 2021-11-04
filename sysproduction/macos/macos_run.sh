7e9bfe720bd86629a103adbe433c317f46121374
Andy Geach <andy@bugorfeature.net>
2021-11-04 17:40:51 +0000

    now evaluates log name from module name

--- sysproduction/macos/macos_run.sh
+++ sysproduction/macos/macos_run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# shellcheck source=/dev/null
 . ~/.profile
-cd $PYSYS_CODE
+cd $PYSYS_CODE || return
 source env/bin/activate
 
-python $SCRIPT_PATH/run.py $1 >> $ECHO_PATH/$2_`date +"%Y-%m-%d_%H%M%S"`.log
+IN=$1
+array=(${IN//./ })
+size=${#array[@]}
+last_index=$((size-1))
+log_file=${array[last_index]//_/$'-'}
+echo "${log_file}"
+
+#python $SCRIPT_PATH/run.py $1 >> $ECHO_PATH/$2_`date +"%Y-%m-%d_%H%M%S"`.log
+python $SCRIPT_PATH/run.py $1 >> $ECHO_PATH/$log_file.log
