6c6b81754b8ce46644baa989b7d620c23bc34ace
Andy Geach <andy@bugorfeature.net>
2021-11-05 08:46:08 +0000

    replication script

---
+++ sysproduction/macos/replicate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# replicates mongodb data from production to development
+
+# fail fast
+set -eo pipefail
+#shopt -s inherit_errexit
+
+source ~/.profile
+
+if [ -d "/Volumes/devuser" ]
+then
+    mongo arctic_development --eval "db.dropDatabase()"
+    mongo development --eval "db.dropDatabase()"
+
+    mongorestore --nsInclude="arctic_production.*" --nsFrom='arctic_production.*' --nsTo='arctic_development.*' /Volumes/devuser/Documents/pst_backup/mongo/
+    mongorestore --nsInclude="production.*" --nsFrom='production.*' --nsTo='development.*' /Volumes/devuser/Documents/pst_backup/mongo/
+
+    exit 0
+else
+    echo "devuser not mounted"
+    exit 1
+fi
