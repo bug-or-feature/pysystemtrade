9826dd3c6d124226d7ccfdd58b19a8c72b304599
Andy Geach <andy@bugorfeature.net>
2021-11-05 08:43:50 +0000

    removing output

--- sysproduction/macos/macos_run.sh
+++ sysproduction/macos/macos_run.sh
@@ -10,7 +10,7 @@ array=(${IN//./ })
 size=${#array[@]}
 last_index=$((size-1))
 log_file=${array[last_index]//_/$'-'}
-echo "${log_file}"
+#echo "${log_file}"
 
 #python $SCRIPT_PATH/run.py $1 >> $ECHO_PATH/$2_`date +"%Y-%m-%d_%H%M%S"`.log
 python $SCRIPT_PATH/run.py $1 >> $ECHO_PATH/$log_file.log
