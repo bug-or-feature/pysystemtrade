bdf61386c229579ff4d9da754edbcbbb8747865d
Andy Geach <andy@bugorfeature.net>
2021-11-09 17:29:03 +0000

    Fixing prod backup source

--- sysproduction/macos/replicate.sh
+++ sysproduction/macos/replicate.sh
@@ -13,8 +13,8 @@ then
     mongo arctic_development --eval "db.dropDatabase()"
     mongo development --eval "db.dropDatabase()"
 
-    mongorestore --nsInclude="arctic_production.*" --nsFrom='arctic_production.*' --nsTo='arctic_development.*' /Volumes/devuser/Documents/pst_backup/mongo/
-    mongorestore --nsInclude="production.*" --nsFrom='production.*' --nsTo='development.*' /Volumes/devuser/Documents/pst_backup/mongo/
+    mongorestore --nsInclude="arctic_production.*" --nsFrom='arctic_production.*' --nsTo='arctic_development.*' /Volumes/devuser/Documents/pst_backup/mongo/mongo_dump
+    mongorestore --nsInclude="production.*" --nsFrom='production.*' --nsTo='development.*' /Volumes/devuser/Documents/pst_backup/mongo/mongo_dump
 
     exit 0
 else
