6a278e50667ca9f9eece46bf3653296c0ea0bae2
Andy Geach <andy@bugorfeature.net>
2021-12-13 15:24:58 +0000

    reverting to upstream version

--- sysproduction/data/instruments.py
+++ sysproduction/data/instruments.py
@@ -11,7 +11,6 @@ from sysobjects.instruments import instrumentCosts
 class dataInstruments(productionDataLayerGeneric):
     def _add_required_classes_to_data(self, data) -> dataBlob:
         data.add_class_object(mongoFuturesInstrumentData)
-        #data.add_class_object(mongoFsbInstrumentData)
         return data
 
     def update_slippage_costs(self, instrument_code: str, new_slippage: float):
@@ -27,13 +26,11 @@ class dataInstruments(productionDataLayerGeneric):
 class diagInstruments(productionDataLayerGeneric):
     def _add_required_classes_to_data(self, data) -> dataBlob:
         data.add_class_object(mongoFuturesInstrumentData)
-        #data.add_class_object(mongoFsbInstrumentData)
         return data
 
     @property
     def db_futures_instrument_data(self) -> futuresInstrumentData:
         return self.data.db_futures_instrument
-        #return self.data.db_fsb_instrument
 
     def get_cost_object(self, instrument_code: str) -> instrumentCosts:
         meta_data = self.get_meta_data(instrument_code)
