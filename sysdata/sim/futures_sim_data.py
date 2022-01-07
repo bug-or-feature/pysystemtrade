0c822afbfac2b2a00b15e5c6f4fa787adcf88057
Andy Geach <andy@bugorfeature.net>
2022-01-07 14:49:53 +0000

    reverting to upstream version

--- sysdata/sim/futures_sim_data.py
+++ sysdata/sim/futures_sim_data.py
@@ -236,6 +236,7 @@ class futuresSimData(simData):
     def get_roll_parameters(self, instrument_code: str) -> rollParameters:
         raise NotImplementedError
 
+
     def get_instrument_object_with_meta_data(
         self, instrument_code: str
     ) -> futuresInstrumentWithMetaData:
