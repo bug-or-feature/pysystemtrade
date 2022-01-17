4bd49e85e2d558e53d4ecf497bd235003a980fbb
Andy Geach <andy@bugorfeature.net>
2022-01-17 20:42:17 +0000

    fsb roll calendar builder adapted to work from Barchart CSV prices

--- sysdata/csv/csv_fsb_contract_prices.py
+++ sysdata/csv/csv_fsb_contract_prices.py
@@ -1,8 +1,7 @@
+from dataclasses import dataclass
 from syscore.dateutils import Frequency
-from syscore.fileutils import get_filename_for_package
 from syscore.objects import arg_not_supplied
 from syscore.pdutils import pd_readcsv
-from sysdata.config.production_config import get_production_config
 from sysdata.csv.csv_futures_contract_prices import (
     csvFuturesContractPriceData,
     ConfigCsvFuturesPrices,
@@ -10,6 +9,11 @@ from sysdata.csv.csv_futures_contract_prices import (
 from syslogdiag.log_to_screen import logtoscreen
 from sysobjects.contracts import futuresContract
 from sysobjects.futures_per_contract_prices import futuresContractPrices
+
+
+@dataclass
+class ConfigCsvFsbPrices(ConfigCsvFuturesPrices):
+    apply_inverse: bool = False
 
 
 class CsvFsbContractPriceData(csvFuturesContractPriceData):
@@ -21,48 +25,29 @@ class CsvFsbContractPriceData(csvFuturesContractPriceData):
     def __init__(
         self,
         datapath=arg_not_supplied,
-        log=logtoscreen("csvFuturesContractPriceData"),
+        log=logtoscreen("CsvFsbContractPriceData"),
         config: ConfigCsvFuturesPrices = arg_not_supplied,
     ):
 
-        prod_config = get_production_config()
-        ig_path = get_filename_for_package(
-            prod_config.get_element_or_missing_data("ig_path")
-        )
-
-        pd_config = ConfigCsvFuturesPrices(
-            input_date_index_name="Date",
-            input_skiprows=0,
-            input_skipfooter=0,
-            input_date_format="%Y-%m-%dT%H:%M:%S%z",
-            input_column_mapping=dict(
-                OPEN=dict(BID="Open.bid", ASK="Open.ask"),
-                HIGH=dict(BID="High.bid", ASK="High.ask"),
-                LOW=dict(BID="Low.bid", ASK="Low.ask"),
-                FINAL=dict(BID="Close.bid", ASK="Close.ask"),
-                VOLUME="Volume",
-            ),
-        )
-
         super().__init__(
-            datapath=ig_path,
-            log=logtoscreen("csvFuturesContractPriceData"),
-            config=pd_config,
+            datapath=datapath,
+            log=log,
+            config=config,
         )
 
     def __repr__(self):
         return f"CsvFsbContractPriceData accessing {self._datapath}"
 
     def _get_prices_for_contract_object_no_checking(
-        self, futures_contract_object: futuresContract
+        self, contract_object: futuresContract
     ) -> futuresContractPrices:
         """
         Read back the prices for a given contract object
 
-        :param contract_object:  futuresContract
+        :param: contract_object:  futuresContract
         :return: data
         """
-        keyname = self._keyname_given_contract_object(futures_contract_object)
+        keyname = self._keyname_given_contract_object(contract_object)
         filename = self._filename_given_key_name(keyname)
         config = self.config
 
@@ -71,6 +56,8 @@ class CsvFsbContractPriceData(csvFuturesContractPriceData):
         input_column_mapping = config.input_column_mapping
         skiprows = config.input_skiprows
         skipfooter = config.input_skipfooter
+        multiplier = config.apply_multiplier
+        inverse = config.apply_inverse
 
         try:
             instrpricedata = pd_readcsv(
@@ -82,19 +69,28 @@ class CsvFsbContractPriceData(csvFuturesContractPriceData):
                 skipfooter=skipfooter,
             )
         except OSError:
-            log = futures_contract_object.log(self.log)
+            log = contract_object.log(self.log)
             log.warning("Can't find adjusted price file %s" % filename)
             return futuresContractPrices.create_empty()
 
         instrpricedata = instrpricedata.groupby(level=0).last()
+        if inverse:
+            instrpricedata = 1.0 / instrpricedata
+        instrpricedata = instrpricedata * multiplier
 
         instrpricedata = futuresContractPrices(instrpricedata)
 
         return instrpricedata
 
-    # def _delete_prices_for_contract_object_with_no_checks_be_careful(self, futures_contract_object: futuresContract):
-    #     pass
-    #
-    # def _get_prices_at_frequency_for_contract_object_no_checking(self, contract_object: futuresContract,
-    #                                                              freq: Frequency) -> futuresContractPrices:
-    #     pass
+    def _delete_prices_for_contract_object_with_no_checks_be_careful(
+            self,
+            futures_contract_object: futuresContract
+    ):
+        pass
+
+    def _get_prices_at_frequency_for_contract_object_no_checking(
+            self,
+            contract_object: futuresContract,
+            freq: Frequency
+    ) -> futuresContractPrices:
+        pass
