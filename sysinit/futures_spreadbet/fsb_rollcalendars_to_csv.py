f7e8ab09d02ade296cdb00a42bc0255c6925a787
Andy Geach <andy@bugorfeature.net>
2022-02-22 09:31:13 +0000

    batch 3 instruments

--- sysinit/futures_spreadbet/fsb_rollcalendars_to_csv.py
+++ sysinit/futures_spreadbet/fsb_rollcalendars_to_csv.py
@@ -161,15 +161,20 @@ if __name__ == "__main__":
     # DONE batch 2
     # "BTP_fsb","DOW_fsb","COFFEE_fsb","EUR_fsb","GILT_fsb","JPY_fsb","NIKKEI_fsb","SOYOIL_fsb"
 
-    # AEX_fsb,ASX_fsb,CAC_fsb,DAX_fsb,FTSE100_fsb,HANG_fsb,OAT_fsb,RUSSELL_fsb,SMI_fsb,SP500_fsb,
-    # AUD_fsb,CHF_fsb,DX_fsb,EURGBP_fsb,GBP_fsb
-    # BOBL_fsb,BUND_fsb,JGB_fsb,SHATZ_fsb,US2_fsb,US5_fsb,US30_fsb,USTB_fsb,
+    # TODO batch 3
+    # "ASX_fsb","HANG_fsb","DX_fsb","GBP_fsb","JGB_fsb","US2_fsb","WHEAT_fsb","SOYBEAN_fsb","COPPER_fsb","SILVER_fsb",
+    # "EUA_fsb","GAS_US_fsb","EDOLLAR_fsb","STERLING3_fsb","VIX_fsb","V2X_fsb"
+
+    # AEX_fsb,CAC_fsb,DAX_fsb,FTSE100_fsb,IBXEX_fsb,JSE40_fsb,MSCISING_fsb,OAT_fsb,RUSSELL_fsb,SMI_fsb,SP500_fsb,SWE30_fsb
+    # AUD_fsb,CHF_fsb,EURGBP_fsb
+    # BOBL_fsb,BUND_fsb,SHATZ_fsb,US5_fsb,US30_fsb,USTB_fsb,
     # COCOA_LDN_fsb,COCOA_NY_fsb,CORN_fsb,COTTON_fsb,LEANHOG_fsb,LIVECOW_fsb,LUMBER_fsb,OATIES
-    #     OJ_fsb,RICE_fsb,ROBUSTA_fsb,SOYBEAN_fsb,SOYMEAL_fsb,SUGAR_fsb,SUGAR11_fsb,WHEAT_fsb,WHEAT_LDN
-    # COPPER_fsb,PALLAD_fsb,PLAT_fsb,SILVER_fsb,
-    # BRENT_W_fsb,EUA_fsb,GAS_US_fsb,GASOIL_fsb,GASOLINE_fsb,HEATOIL_fsb
-    # EDOLLAR_fsb,EURIBOR_fsb,STERLING3_fsb,
-    # VIX_fsb,V2X_fsb
+    #     OJ_fsb,RICE_fsb,ROBUSTA_fsb,SOYMEAL_fsb,SUGAR_fsb,SUGAR11_fsb,WHEAT_LDN
+    # PALLAD_fsb,PLAT_fsb,
+    # BRENT_W_fsb,GASOIL_fsb,GASOLINE_fsb,HEATOIL_fsb
+    # EURIBOR_fsb,
+    #
+    # FTSE_Mid_250, FTSE_350_*, Techmark, Germany_Tech_30, Germany_Mid_Cap_50
 
     instr_code = "GILT_fsb"
 
