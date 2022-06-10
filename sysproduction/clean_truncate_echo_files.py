d5de74a1aa08d0a37b092bddedc08b988371f973
Andy Geach <andy@bugorfeature.net>
2022-06-10 17:07:13 +0100

    echo clean debug messages

--- sysproduction/clean_truncate_echo_files.py
+++ sysproduction/clean_truncate_echo_files.py
@@ -21,11 +21,11 @@ class cleanTruncateEchoFiles:
     def clean_echo_files(self):
         pathname = get_echo_file_directory()
         echo_extension = get_echo_extension()
-        self.data.log.msg("Archiving echo files")
+        self.data.log.msg(f"Archiving echo files (*.{echo_extension}) from {pathname}")
         rename_files_with_extension_in_pathname_as_archive(
             pathname, extension=echo_extension, new_extension=".arch"
         )
-        self.data.log.msg("Deleting old echo files")
+        self.data.log.msg(f"Deleting old echo files (*.arch) from {pathname}")
         delete_old_files_with_extension_in_pathname(
             pathname, extension=".arch", days_old=30
         )
