52f5a796e3a3cc4122a4f1c5a886feb8ae3e883f
Andy Geach <andy@bugorfeature.net>
2022-06-16 19:51:10 +0100

    init scripts adjusted

--- sysinit/futures_cj/norgate_futures_contract_prices.py
+++ sysinit/futures_cj/norgate_futures_contract_prices.py
@@ -318,8 +318,8 @@ if __name__ == "__main__":
 
     # rename/move files, just for one (Norgate style) instrument code. Operates in 'dry_run' mode by default
     # to actually do the rename, set dry_run=False
-    #rename_files(datapath, "NKD")
-    rename_files(datapath, "NKD", dry_run=False)
+    # rename_files(datapath, "NKD")
+    # rename_files(datapath, "NKD", dry_run=False)
 
     # rename/move all files. Operates in 'dry_run' mode by default
     # to actually do the rename, set dry_run=False
@@ -345,8 +345,8 @@ if __name__ == "__main__":
     #for instr in ["BOBL", "BTP", "BUND", "BUXL", "OAT", "SHATZ"]:
     #for instr in ['COTTON2']:
     #for instr in ['EURIBOR']:
-    #for instr in ['NIKKEI']:
-    #     transfer_norgate_prices_to_arctic_single(instr, datapath=datapath)
+    for instr in ['NIKKEI']:
+         transfer_norgate_prices_to_arctic_single(instr, datapath=datapath)
          #transfer_barchart_prices_to_arctic_single(instr, datapath=datapath)
 
     # import all contract files for all instruments
