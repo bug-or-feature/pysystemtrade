021a2981fbb598c159e54bbdc0739ff14575219c
Andy Geach <andy@bugorfeature.net>
2022-06-16 14:58:34 +0100

    also delete spread data

--- data/tools/delete_instrument.py
+++ data/tools/delete_instrument.py
@@ -1,6 +1,7 @@
 from sysdata.arctic.arctic_adjusted_prices import arcticFuturesAdjustedPricesData
 from sysdata.arctic.arctic_futures_per_contract_prices import arcticFuturesContractPriceData
 from sysdata.arctic.arctic_multiple_prices import arcticFuturesMultiplePricesData
+from sysdata.arctic.arctic_spreads import arcticSpreadsForInstrumentData
 from sysdata.data_blob import dataBlob
 from sysproduction.data.prices import get_valid_instrument_code_from_user
 from syscore.interactive import true_if_answer_is_yes
@@ -18,7 +19,8 @@ def delete_instrument():
                 do_another = False
             else:
                 if true_if_answer_is_yes(
-                    f"OK to delete all contract, multiple and adjusted prices for {instr}, are you sure? (y/n):"
+                    f"OK to delete all contract, multiple, adjusted and spread price data for {instr}, "
+                    f"are you sure? (y/n):"
                 ):
                     # Delete contract data
                     contract_data = arcticFuturesContractPriceData()
@@ -32,7 +34,11 @@ def delete_instrument():
                     adjusted_prices = arcticFuturesAdjustedPricesData()
                     adjusted_prices.delete_adjusted_prices(instr, are_you_sure=True)
 
-                    print(f"Contract, multiple and adjusted price data for {instr} deleted.")
+                    # delete spread data
+                    spread_data = arcticSpreadsForInstrumentData()
+                    spread_data.delete_spreads(instr, are_you_sure=True)
+
+                    print(f"Contract, multiple, adjusted and spread price data for {instr} deleted.")
 
                 else:
                     print("Deletion not confirmed, no action")
