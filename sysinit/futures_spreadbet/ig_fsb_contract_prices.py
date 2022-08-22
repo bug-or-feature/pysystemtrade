2ac86676ac5be73a1a77a11cbc7fbefb213486f1
Andy Geach <andy@bugorfeature.net>
2022-08-22 18:28:08 +0100

    init scripts updated

--- sysinit/futures_spreadbet/ig_fsb_contract_prices.py
+++ sysinit/futures_spreadbet/ig_fsb_contract_prices.py
@@ -49,10 +49,8 @@ if __name__ == "__main__":
 
     # single instrument
     # for instr in ["GOLD_fsb"]:
-    #for instr in ['BUXL_fsb', 'GOLD_fsb', 'NASDAQ_fsb', 'NZD_fsb', 'US10_fsb']:
-    for instr in ['ASX_fsb', 'BTP_fsb', 'CAD_fsb', 'COFFEE_fsb', 'COPPER_fsb', 'CRUDE_W_fsb', 'DOW_fsb', 'DX_fsb',
-                  'EUA_fsb', 'EUROSTX_fsb', 'EUR_fsb', 'GAS_US_fsb', 'GBP_fsb', 'GILT_fsb', 'HANG_fsb', 'JGB_fsb',
-                  'JPY_fsb', 'NIKKEI_fsb', 'SILVER_fsb', 'SOYBEAN_fsb', 'SOYOIL_fsb', 'US2_fsb', 'V2X_fsb', 'WHEAT_fsb']:
+    for instr in ['AEX_fsb', 'AUD_fsb', 'CAC_fsb', 'CHF_fsb', 'DAX_fsb', 'FTSE100_fsb', 'IBXEX_fsb',
+                  'MSCISING_fsb', 'RUSSELL_fsb', 'SMI_fsb', 'SP500_fsb']:
         transfer_ig_prices_to_arctic_single(instr, datapath=datapath)
 
     # all instruments
