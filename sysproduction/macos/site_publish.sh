eb6255dfb2268994fe4f2dd847858ff7fdcc5422
Andy Geach <andy@bugorfeature.net>
2022-08-25 16:53:16 +0100

    site updates

--- sysproduction/macos/site_publish.sh
+++ sysproduction/macos/site_publish.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/zsh
 
-. ~/.profile
+. ~/.zprofile
 cd $PYSYS_CODE || return
+for f in site/reports/*_report; do mv "$f" "$f.txt"; done
 git add site/*
 git add site/reports/*
 git commit -m "updating site"
