eecf3c43c19a1f6325574fce80b13c8652ad9e12
Andy Geach <andy@bugorfeature.net>
2022-10-10 12:20:33 +0100

    including dashboard

--- sysproduction/linux/scripts/push_site.sh
+++ sysproduction/linux/scripts/push_site.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
 cd ~/harbor-macro/harbor-macro.gitlab.io/
+git add public/*
 git add public/reports/*.txt
-git commit -m "Updating reports"
+git commit -m "Updating site"
 
 git push origin
 
