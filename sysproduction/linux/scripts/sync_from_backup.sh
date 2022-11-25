69086defb0a883e6f2d99f48614a1ff92219ae5a
Andy Geach <andy@bugorfeature.net>
2022-11-25 16:55:32 +0000

    sync from backup

--- sysproduction/linux/scripts/sync_from_backup.sh
+++ sysproduction/linux/scripts/sync_from_backup.sh
@@ -6,13 +6,13 @@ DEST="/home/caleb/pysystemtrade/data/futures_cj";
 echo "`date "+%Y-%m-%d %H:%M:%S"` Syncing backup files, source: '$SOURCE', dest: '$DEST'"
 
 echo "`date "+%Y-%m-%d %H:%M:%S"` Backing up adjusted prices"
-rsync -av $SOURCE/adjusted_prices $DEST/adjusted_prices_csv
+rsync -av $SOURCE/adjusted_prices/*.csv $DEST/adjusted_prices_csv
 
 echo "`date "+%Y-%m-%d %H:%M:%S"` Backing up multiple prices"
-rsync -av $SOURCE/multiple_prices $DEST/multiple_prices_csv
+rsync -av $SOURCE/multiple_prices/*.csv $DEST/multiple_prices_csv
 
 echo "`date "+%Y-%m-%d %H:%M:%S"` Backing up fx prices"
-rsync -av $SOURCE/fx_prices $DEST/fx_prices_csv
+rsync -av $SOURCE/fx_prices/*.csv $DEST/fx_prices_csv
 
 echo "`date "+%Y-%m-%d %H:%M:%S"` Finished sync of backup files to data"
 
