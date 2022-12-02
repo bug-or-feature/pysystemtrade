9cdbdab717f101e69c8ba1f30f6829eb965d7e36
Andy Geach <andy@bugorfeature.net>
2022-12-02 14:54:14 +0000

    including reconcile and strategy reports

--- sysproduction/linux/scripts/manage_report_files.sh
+++ sysproduction/linux/scripts/manage_report_files.sh
@@ -9,6 +9,8 @@ echo "`date "+%Y-%m-%d %H:%M:%S"` Starting archive of today's ($TODAY) report fi
 cp -v $REPORTS_DIR/Status_report.txt $REPORTS_DIR/older/Status_report_$TODAY.txt
 cp -v $REPORTS_DIR/Trade_report.txt $REPORTS_DIR/older/Trade_report_$TODAY.txt
 cp -v $REPORTS_DIR/Roll_report.txt $REPORTS_DIR/older/Roll_report_$TODAY.txt
+cp -v $REPORTS_DIR/Reconcile_report.txt $REPORTS_DIR/older/Reconcile_report_$TODAY.txt
+cp -v $REPORTS_DIR/Strategy_report.txt $REPORTS_DIR/older/Strategy_report_$TODAY.txt
 
 #cp -v $REPORTS_DIR/'P&L_report.txt' $REPORTS_DIR/older/'P&L_report'_$TODAY.txt
 #cp -v $REPORTS_DIR/'Trading_Rule_P&L.txt.pdf' $REPORTS_DIR/older/'Trading_Rule_P&L_'$TODAY.pdf
@@ -19,11 +21,9 @@ cp -v $REPORTS_DIR/Roll_report.txt $REPORTS_DIR/older/Roll_report_$TODAY.txt
 #cp -v $REPORTS_DIR/Liquidity_report.txt $REPORTS_DIR/older/Liquidity_report_$TODAY.txt
 #cp -v $REPORTS_DIR/Market_monitor_report.txt $REPORTS_DIR/older/Market_monitor_report_$TODAY.txt
 #cp -v $REPORTS_DIR/Minimum_capital_report.txt $REPORTS_DIR/older/Minimum_capital_report_$TODAY.txt
-#cp -v $REPORTS_DIR/Reconcile_report.txt $REPORTS_DIR/older/Reconcile_report_$TODAY.txt
 #cp -v $REPORTS_DIR/Remove_markets_report.txt $REPORTS_DIR/older/Remove_markets_report_$TODAY.txt
 #cp -v $REPORTS_DIR/Risk_report.txt $REPORTS_DIR/older/Risk_report_$TODAY.txt
 #cp -v $REPORTS_DIR/Slippage_report.txt $REPORTS_DIR/older/Slippage_report_$TODAY.txt
-#cp -v $REPORTS_DIR/Strategy_report.txt $REPORTS_DIR/older/Strategy_report_$TODAY.txt
 
 echo "`date "+%Y-%m-%d %H:%M:%S"` Finished archive of old report files"
 echo ""
