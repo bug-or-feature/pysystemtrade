f4f7cf1037ba166a2c88c8bd5b60fe86927c67a2
Andy Geach <andy@bugorfeature.net>
2022-12-05 11:06:36 +0000

    only need to keep older strategy and trade reports

--- sysproduction/linux/scripts/manage_report_files.sh
+++ sysproduction/linux/scripts/manage_report_files.sh
@@ -6,12 +6,11 @@ TODAY=`date "+%Y-%m-%d"`
 echo ""
 echo "`date "+%Y-%m-%d %H:%M:%S"` Starting archive of today's ($TODAY) report files..."
 
-cp -v $REPORTS_DIR/Status_report.txt $REPORTS_DIR/older/Status_report_$TODAY.txt
 cp -v $REPORTS_DIR/Trade_report.txt $REPORTS_DIR/older/Trade_report_$TODAY.txt
-cp -v $REPORTS_DIR/Roll_report.txt $REPORTS_DIR/older/Roll_report_$TODAY.txt
-cp -v $REPORTS_DIR/Reconcile_report.txt $REPORTS_DIR/older/Reconcile_report_$TODAY.txt
 cp -v $REPORTS_DIR/Strategy_report.txt $REPORTS_DIR/older/Strategy_report_$TODAY.txt
-
+#cp -v $REPORTS_DIR/Status_report.txt $REPORTS_DIR/older/Status_report_$TODAY.txt
+#cp -v $REPORTS_DIR/Roll_report.txt $REPORTS_DIR/older/Roll_report_$TODAY.txt
+#cp -v $REPORTS_DIR/Reconcile_report.txt $REPORTS_DIR/older/Reconcile_report_$TODAY.txt
 #cp -v $REPORTS_DIR/'P&L_report.txt' $REPORTS_DIR/older/'P&L_report'_$TODAY.txt
 #cp -v $REPORTS_DIR/'Trading_Rule_P&L.txt.pdf' $REPORTS_DIR/older/'Trading_Rule_P&L_'$TODAY.pdf
 #cp -v $REPORTS_DIR/Costs_report.txt $REPORTS_DIR/older/Costs_report_$TODAY.txt
