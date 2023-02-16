0f6d5054fd6d037ce80e8f71a494e280cbf11e26
Andy Geach <andy@bugorfeature.net>
2023-02-16 13:26:04 +0000

    replication script tweaked for new Sync dir

--- sysproduction/macos/replicate_v4.sh
+++ sysproduction/macos/replicate_v4.sh
@@ -13,8 +13,8 @@ then
     mongo arctic_development_v4 --eval "db.dropDatabase()"
     mongo development_v4 --eval "db.dropDatabase()"
 
-    mongorestore --nsInclude="arctic_production_v4.*" --nsFrom='arctic_production_v4.*' --nsTo='arctic_development_v4.*' /Volumes/devuser/Documents/pst_backup/mongo/mongo_dump
-    mongorestore --nsInclude="production_v4.*" --nsFrom='production_v4.*' --nsTo='development_v4.*' /Volumes/devuser/Documents/pst_backup/mongo/mongo_dump
+    mongorestore --nsInclude="arctic_production_v4.*" --nsFrom='arctic_production_v4.*' --nsTo='arctic_development_v4.*' /Volumes/devuser/Sync/pst/mongo/mongo_dump
+    mongorestore --nsInclude="production_v4.*" --nsFrom='production_v4.*' --nsTo='development_v4.*' /Volumes/devuser/Sync/pst/mongo/mongo_dump
 
     exit 0
 else
