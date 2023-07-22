866d83fa37b82acb0270289227f8278ac1a3323f
Andy Geach <andy@bugorfeature.net>
2023-07-22 09:56:16 +0100

    new super simple market algo for FSBs - only difference is that it does not cut down sizes

---
+++ sysexecution/algos/algo_market_fsb.py
@@ -0,0 +1,23 @@
+from sysexecution.algos.algo_market import algoMarket
+from sysexecution.order_stacks.broker_order_stack import orderWithControls
+
+
+class algoMarketFsb(algoMarket):
+    """
+    Simplest possible market order execution method for FSBs. The only difference
+    with the standard market algo is that we don't cut down the size
+    """
+
+    def prepare_and_submit_trade(self) -> orderWithControls:
+        contract_order = self.contract_order
+        log = contract_order.log_with_attributes(self.data.log)
+
+        log.debug(f"Not changing size {str(contract_order.trade)}, we are an FSB")
+
+        broker_order_with_controls = (
+            self.get_and_submit_broker_order_for_contract_order(
+                contract_order, order_type=self.order_type_to_use
+            )
+        )
+
+        return broker_order_with_controls
