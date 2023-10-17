ce315303ce506f835016b979bde0becdeafd931f
Andy Geach <andy@bugorfeature.net>
2023-10-17 17:34:58 +0100

    remove empty risk_overlay config from example tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -51,7 +51,6 @@ def my_config(ewmac_8, ewmac_32):
     my_config.trading_rules = dict(ewmac8=ewmac_8, ewmac32=ewmac_32)
     my_config.instruments = ["US10", "EDOLLAR", "CORN", "SP500"]
     my_config.notional_trading_capital = 1000000  # TODO fix local pytest run
-    my_config.risk_overlay = arg_not_supplied
     my_config.notional_trading_capital = 1000000
     my_config.exclude_instrument_lists = dict(
         ignore_instruments=["MILK"],
@@ -310,7 +309,6 @@ class TestExamples:
                 percentage_vol_target=25.00,
                 notional_trading_capital=500000,
                 base_currency="GBP",
-                risk_overlay=arg_not_supplied,
                 exclude_instrument_lists=dict(
                     ignore_instruments=["MILK"],
                     trading_restrictions=["BUTTER"],
@@ -380,7 +378,6 @@ class TestExamples:
     def test_simple_system_config_import(self, data):
 
         my_config = Config("systems.provided.example.simplesystemconfig.yaml")
-        my_config.risk_overlay = arg_not_supplied
         my_config.exclude_instrument_lists = dict(
             ignore_instruments=["MILK"],
             trading_restrictions=["BUTTER"],
@@ -419,7 +416,6 @@ class TestExamples:
         This is the simple system from 'examples.introduction.prebakedsimplesystems'
         """
         my_system = simplesystem()
-        my_system.config.risk_overlay = arg_not_supplied
         print(my_system)
         print(my_system.portfolio.get_notional_position("EDOLLAR").tail(5))
 
@@ -429,7 +425,6 @@ class TestExamples:
         This is the config system from 'examples.introduction.prebakedsimplesystems'
         """
         my_config = Config("systems.provided.example.simplesystemconfig.yaml")
-        my_config.risk_overlay = arg_not_supplied
         my_data = csvFuturesSimData()
         my_system = simplesystem(config=my_config, data=my_data)
         print(my_system.portfolio.get_notional_position("EDOLLAR").tail(5))
@@ -441,7 +436,6 @@ class TestExamples:
         but without graph plotting
         """
         system = base_futures_system()
-        system.config.risk_overlay = arg_not_supplied
         print(system.accounts.portfolio().sharpe())
 
     @staticmethod
