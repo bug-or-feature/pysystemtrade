7cdc20e882d3a9b550fbcbd0bad6e81d1751c6f1
Andy Geach <andy@bugorfeature.net>
2024-03-02 17:45:22 +0000

    also allow adjusted price comparison with Norgate data

--- sysproduction/reporting/adhoc/compare_adjusted_prices.py
+++ sysproduction/reporting/adhoc/compare_adjusted_prices.py
@@ -20,20 +20,29 @@ def compare_adjusted_prices(instr, draw=False):
     )
 
     fut_data = csvFuturesSimData()
-    fut_code = remove_suffix(instr, "_fsb")
+
+    norgate_data = csvFuturesSimData(
+        csv_data_paths=dict(
+            csvFuturesMultiplePricesData="/Users/ageach/Documents/backup/pst_caleb/csv/backups_csv/multiple_prices",
+            csvFuturesAdjustedPricesData="/Users/ageach/Documents/backup/pst_caleb/csv/backups_csv/adjusted_prices",
+        )
+    )
     import_config = build_import_config(instr)
 
     fsb_df = fsb_data.db_futures_adjusted_prices_data.get_adjusted_prices(instr)
+    fut_code = remove_suffix(instr, "_fsb")
     fut_df = fut_data.db_futures_adjusted_prices_data.get_adjusted_prices(fut_code)
+    nor_df = norgate_data.db_futures_adjusted_prices_data.get_adjusted_prices(fut_code)
     fsb_df = fsb_df.div(import_config.apply_multiplier)
 
     if draw:
         plt.title(f"FSB v Future adjusted prices for {instr}")
-        plt.plot(fsb_df, label="FSB")
-        plt.plot(fut_df, label="Future")
+        plt.plot(fsb_df["2000-01-01":], label="FSB")
+        plt.plot(fut_df["2000-01-01":], label="Future")
+        plt.plot(nor_df["2000-01-01":], label="Norgate")
         plt.legend()
         show()
 
 
 if __name__ == "__main__":
-    compare_adjusted_prices("CRUDE_W_fsb", draw=True)
+    compare_adjusted_prices("DOW_fsb", draw=True)
