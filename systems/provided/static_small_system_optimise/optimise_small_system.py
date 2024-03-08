7866d117060f5f8f171aff42119239abd0442f4d
Andy Geach <andy@bugorfeature.net>
2024-03-08 09:01:28 +0000

    static selection

--- systems/provided/static_small_system_optimise/optimise_small_system.py
+++ systems/provided/static_small_system_optimise/optimise_small_system.py
@@ -319,34 +319,19 @@ def net_SR_for_instrument(
     )
 
 
-def size_penalty(instr_code, maximum_position, min_bet=1.0):
-    if maximum_position < 0.5:
-        return 9999
-
-    return 0.125 / maximum_position**2
-
-
-# def size_penalty(instr_code, maximum_position, min_bet=1.0):
+# def size_penalty(maximum_position):
 #     if maximum_position < 0.5:
 #         return 9999
 #
 #     return 0.125 / maximum_position**2
 
 
-# def size_penalty(instr_code, maximum_position, min_bet=1.0):
-#     multiple_of_min_bet = maximum_position / min_bet
-#     if maximum_position < multiple_of_min_bet:
-#         return 9999
-#     else:
-#         return 0.125 / multiple_of_min_bet**2
-
-
-# def size_penalty(instr_code, maximum_position, min_bet=1.0):
-#     # multiple_of_min_bet = maximum_position / minimum_position
-#     if maximum_position < (min_bet * 0.5):
-#         return 9999
-#     else:
-#         return 0.125 / maximum_position**2
+def size_penalty(instr_code, maximum_position, min_bet=1.0):
+    multiple_of_min_bet = maximum_position / min_bet
+    if multiple_of_min_bet < 0.5:
+        return 9999
+    else:
+        return 0.125 / multiple_of_min_bet**2
 
 
 # override this for non futures
