e5223dbc74843f42ab734835c4b3806ee7b28c71
Andy Geach <andy@bugorfeature.net>
2024-06-04 14:06:50 +0100

    system config tweaks

--- systems/futures_spreadbet/run_fsb_static_system.py
+++ systems/futures_spreadbet/run_fsb_static_system.py
@@ -8,11 +8,15 @@ from syslogging.logger import get_logger
 from sysdata.config.configdata import Config
 from syscore.pdutils import print_full
 
-SAVED_SYSTEM = "systems.futures_spreadbet.saved-system.pck"
-# CONFIG = "systems.futures_spreadbet.config.fsb_static_system_v5_6.yaml"
+# SAVED_SYSTEM = "systems.futures_spreadbet.saved-system-min.pck"
 # CONFIG = "systems.futures_spreadbet.config.fsb_static_minimal.yaml"
-# CONFIG = "systems.futures_spreadbet.config.fsb_static_system_full_hier.yaml"
-CONFIG = "systems.futures_spreadbet.config.fsb_static_system_full_est.yaml"
+
+# SAVED_SYSTEM = "systems.futures_spreadbet.saved-system.pck"
+# CONFIG = "systems.futures_spreadbet.config.fsb_static_system_full_est.yaml"
+
+CONFIG = "systems.futures_spreadbet.config.fsb_static_system_mid_est.yaml"
+SAVED_SYSTEM = "systems.futures_spreadbet.config.fsb_static_system_mid_est.pck"
+
 
 log = get_logger("backtest")
 
@@ -38,77 +42,13 @@ def run_system(
             config.use_forecast_scale_estimates = True
         system = fsb_static_system(config=config)
 
-    # acc_portfolio = system.accounts.portfolio()
-    acc_portfolio = system.accounts.portfolio(roundpositions=False)
-    # acc_portfolio_percent = system.accounts.portfolio().percent
-    acc_portfolio_percent = system.accounts.portfolio(roundpositions=False).percent
-
-    # weights_df = system.portfolio.get_instrument_weights()
-    # weights = weights_df.iloc[-1]
-    # print_full(weights_df)
-
-    # for instr in system.get_instrument_list():
-    #     cf = round(system.combForecast.get_combined_forecast(instr)[-1], 2)
-    #     sub_pos = round(system.positionSize.get_subsystem_position(instr)[-1], 2)
-    #     not_pos = round(system.portfolio.get_notional_position(instr)[-1], 2)
-    #     act_pos = round(system.portfolio.get_actual_position(instr)[-1], 2)
-    #     buffer = round(system.portfolio.get_buffers(instr).iloc[-1], 3)
-    #     pos_buffers = system.portfolio.get_buffers_for_position(instr).iloc[-1]
-    #     top = round(pos_buffers["top_pos"], 2)
-    #     bot = round(pos_buffers["bot_pos"], 2)
-    #     print(
-    #         f"{instr=}: {cf=}, {sub_pos=}, {not_pos=}, {act_pos=}, {buffer=}, {bot=}, {top=}"
-    #     )
-
-    # pos = system.portfolio.get_notional_position("DOW_fsb")
-    # system.accounts.pandl_for_instrument("DOW_fsb").curve().plot(legend=True)
-    # show()
-    # system.accounts.pandl_for_instrument("HANG_fsb").curve().plot(legend=True)
-    # show()
-    # system.accounts.pandl_for_instrument("LEANHOG_fsb").curve().plot(legend=True)
-    # show()
-    # system.accounts.pandl_for_instrument("US30_fsb").curve().plot(legend=True)
-    # show()
-    # log.info(f"pos: {pos}")
-
-    # forecast_sc = system.forecastScaleCap
-    # comb_forecast = system.combForecast
-    # positions = system.positionSize
-    # portfolio = system.portfolio
-    # accounts = system.accounts
+    acc_portfolio_percent = system.accounts.portfolio().percent
 
     log.info(f"Stats: {acc_portfolio_percent.stats()}")
     log.info(f"Stats as %: {acc_portfolio_percent.stats()}\n")
 
-    # acc_portfolio.curve().plot(legend=True)
-    # show()
-    # acc_pf_nr.curve().plot()
     acc_portfolio_percent.curve().plot(legend=True)
     show()
-
-    # for instr in system.portfolio.get_instrument_list(
-    #     for_instrument_weights=True, auto_remove_bad_instruments=True
-    # ):
-    # for instr in [
-    #     "BTP_fsb",
-    #     "DOW_fsb",
-    #     "EUA_fsb",
-    #     "EUR_fsb",
-    #     "GOLD_fsb",
-    #     "LEANHOG_fsb",
-    #     "ROBUSTA_fsb",
-    # ]:
-    # instr_pnl = system.accounts.pandl_for_instrument(instr, roundpositions=False)
-    # instr_pnl = system.accounts.pandl_for_instrument(instr)
-    # system.accounts.pandl_across_subsystems(instr, roundpositions=False).plot(
-    # system.accounts.pandl_for_subsystem(instr, roundpositions=False).plot(
-    #     title=instr
-    # )
-    # show()
-    # system.accounts.pandl_for_instrument(instr, roundpositions=False)
-    # ss_pnl = system.accounts.pandl_for_subsystem(instr)
-    # print(f"{instr}: {instr_pnl.stats()}")
-    # print(f"{instr}: {instr_pnl.sharpe()}")
 
     if write_pickle:
         write_pickle_file(system)
