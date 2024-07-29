272f8293b6aff20cacfa7450e46ba46e38f023ab
Andy Geach <andy@bugorfeature.net>
2024-07-29 11:44:02 +0100

    rounding tweaks

--- systems/futures_spreadbet/dynamic_small_system_optimise/buffering.py
+++ systems/futures_spreadbet/dynamic_small_system_optimise/buffering.py
@@ -11,12 +11,10 @@ def adjust_weights_with_factor(
     desired_trades_weight_space = optimised_weights_as_np - prior_weights_as_np
     adjusted_trades_weight_space = adj_factor * desired_trades_weight_space
 
-    rounded_adjusted_trades_as_weights = (
-        calculate_adjusting_trades_rounding_in_minimum_bet_space(
-            adjusted_trades_weight_space=adjusted_trades_weight_space,
-            per_contract_value_as_np=per_contract_value_as_np,
-            min_bets_as_np=min_bets_as_np,
-        )
+    rounded_adjusted_trades_as_weights = calc_adj_trades_round_nearest_multiple(
+        adjusted_trades_weight_space=adjusted_trades_weight_space,
+        per_contract_value_as_np=per_contract_value_as_np,
+        min_bets_as_np=min_bets_as_np,
     )
 
     new_optimal_weights = prior_weights_as_np + rounded_adjusted_trades_as_weights
@@ -24,7 +22,28 @@ def adjust_weights_with_factor(
     return new_optimal_weights
 
 
-def calculate_adjusting_trades_rounding_in_minimum_bet_space(
+def calc_adj_trades_round_nearest_multiple(
+    adjusted_trades_weight_space: np.array,
+    per_contract_value_as_np: np.array,
+    min_bets_as_np: np.array,
+) -> np.array:
+    # convert weights to positions
+    adj_trades = adjusted_trades_weight_space / per_contract_value_as_np
+
+    # round to nearest multiple of minimum bet
+    rounded_adjusted_trades = np.round(
+        np.round(adj_trades / min_bets_as_np) * min_bets_as_np, 2
+    )
+
+    # convert positions back to weights
+    rounded_adjusted_trades_as_weights = (
+        rounded_adjusted_trades * per_contract_value_as_np
+    )
+
+    return rounded_adjusted_trades_as_weights
+
+
+def calc_adj_trades_round_nearest_penny(
     adjusted_trades_weight_space: np.array,
     per_contract_value_as_np: np.array,
     min_bets_as_np: np.array,
