9c48791f71a5481ddf22f9c68bf428c5801aa2ba
Andy Geach <andy@bugorfeature.net>
2024-08-06 17:55:20 +0100

    run_system_classic reverted to upstream

--- sysproduction/strategy_code/run_system_classic.py
+++ sysproduction/strategy_code/run_system_classic.py
@@ -23,7 +23,6 @@ from sysobjects.production.tradeable_object import instrumentStrategy
 from sysproduction.data.currency_data import dataCurrency
 from sysproduction.data.capital import dataCapital
 from sysproduction.data.contracts import dataContracts
-from sysproduction.data.fsb_instruments import diagFsbInstruments
 from sysproduction.data.optimal_positions import dataOptimalPositions
 from sysproduction.data.sim_data import get_sim_data_object_for_production
 
@@ -148,14 +147,13 @@ def updated_buffered_positions(data: dataBlob, strategy_name: str, system: Syste
     log = data.log
 
     data_optimal_positions = dataOptimalPositions(data)
-    diag_instruments = diagFsbInstruments(data)
 
     list_of_instruments = system.get_instrument_list()
     for instrument_code in list_of_instruments:
         lower_buffer, upper_buffer = get_position_buffers_from_system(
             system, instrument_code
         )
-        pos = construct_position_entry(
+        position_entry = construct_position_entry(
             data=data,
             system=system,
             instrument_code=instrument_code,
@@ -166,12 +164,11 @@ def updated_buffered_positions(data: dataBlob, strategy_name: str, system: Syste
             instrument_code=instrument_code, strategy_name=strategy_name
         )
         data_optimal_positions.update_optimal_position_for_instrument_strategy(
-            instrument_strategy=instrument_strategy, position_entry=pos
+            instrument_strategy=instrument_strategy, position_entry=position_entry
         )
-        min_bet = diag_instruments.get_minimum_bet(instrument_code, data.log.name)
         log.debug(
-            f"New buffered positions {pos.lower_position:.2f} "
-            f"{pos.upper_position:.2f} (min bet: {min_bet})",
+            "New buffered positions %.3f %.3f"
+            % (position_entry.lower_position, position_entry.upper_position),
             instrument_code=instrument_code,
         )
 
