034f9ffeea30a60593a47c791f1fb667102c0a76
Andy Geach <andy@bugorfeature.net>
2024-08-22 11:16:17 +0100

    EURIBOR-ICE and SONIA3 roll config tweaked. again

--- sysinit/futures_spreadbet/fsb_multipleprices.py
+++ sysinit/futures_spreadbet/fsb_multipleprices.py
@@ -198,7 +198,7 @@ if __name__ == "__main__":
     # only change if you have written the files elsewhere
     csv_roll_data_path = "fsb.roll_calendars_csv"
 
-    for instr in ["SGD_fsb", "EURIBOR-ICE_fsb", "SONIA3_fsb"]:
+    for instr in ["EURIBOR-ICE_fsb", "SONIA3_fsb"]:
         process_multiple_prices_single_instrument(
             instrument_code=instr,
             adjust_calendar_to_prices=False,
