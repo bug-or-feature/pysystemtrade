a3ffd571e862908cfdbc101cc6c955761833c700
Andy Geach <andy@bugorfeature.net>
2024-09-08 22:14:46 +0100

    check spread on creating brokers orders, cancel if too wide

--- sysexecution/algos/algo_market_fsb.py
+++ sysexecution/algos/algo_market_fsb.py
@@ -1,14 +1,41 @@
+from syscore.exceptions import missingData, spreadTooWide
+from sysdata.data_blob import dataBlob
+from sysdata.mongodb.mongo_spread_costs import mongoSpreadCostData
+from sysexecution.tick_data import tickerObject
+from sysexecution.algos.algo import benchmarkPriceCollection
 from sysexecution.algos.algo_market import algoMarket
 from sysexecution.order_stacks.broker_order_stack import orderWithControls
+from sysexecution.orders.contract_orders import contractOrder
 from sysexecution.orders.named_order_objects import missing_order
-from syscore.exceptions import missingData
 
 
 class algoMarketFsb(algoMarket):
     """
-    Simplest possible market order execution method for FSBs. The only difference
-    with the standard market algo is that we don't cut down the size
+    Simplest possible market order execution method for FSBs. Differences:
+      - we don't cut down the size
+      - we check the spread is not too wide
     """
+
+    def __init__(self, data: dataBlob, contract_order: contractOrder):
+        super().__init__(data, contract_order)
+        data.add_class_object(mongoSpreadCostData)
+
+    def validate(
+        self,
+        contract_order: contractOrder,
+        ticker_object: tickerObject,
+        prices: benchmarkPriceCollection,
+    ):
+        instr_code = contract_order.instrument_code
+        slippage = self.data.db_spread_cost.get_spread_cost(instr_code)
+        expected_spread = slippage * 2 * 1.05
+        actual_spread = ticker_object.last_tick_analysis.spread
+        if actual_spread > expected_spread:
+            msg = (
+                f"Actual spread ({actual_spread}) for {instr_code} is "
+                f"higher than expected ({expected_spread})"
+            )
+            raise spreadTooWide(msg)
 
     def prepare_and_submit_trade(self) -> orderWithControls:
         contract_order = self.contract_order
