a4041f785b5b6aa38fd1105b0f02f1738b3e22c1
Andy Geach <andy@bugorfeature.net>
2024-10-14 18:13:50 +0100

    clone large to small

--- sysinit/futures/clone_large_to_small_contracts.py
+++ sysinit/futures/clone_large_to_small_contracts.py
@@ -3,17 +3,19 @@ from sysinit.futures.clone_data_for_instrument import clone_data_for_instrument
 
 # format is 'from' = 'to'
 mapping_dict = dict(
-    COPPER="COPPER-micro",
-    CRUDE_W="CRUDE_W_micro",
-    EUR="EUR_micro",
-    GAS_US="GAS_US_mini",
-    GOLD="GOLD_micro",
-    HANG="HANG_mini",
-    KOSPI="KOSPI_mini",
-    NASDAQ="NASDAQ_micro",
-    SOYBEAN="SOYBEAN_mini",
-    SP500="SP500_micro",
-    US30U="US30",
+    AUD="AUD_micro",
+    # COPPER="COPPER-micro",
+    # CRUDE_W="CRUDE_W_micro",
+    # EUR="EUR_micro",
+    # GAS_US="GAS_US_mini",
+    # GOLD="GOLD_micro",
+    # HANG="HANG_mini",
+    JPY="JPY_mini",
+    # KOSPI="KOSPI_mini",
+    # NASDAQ="NASDAQ_micro",
+    # SOYBEAN="SOYBEAN_mini",
+    # SP500="SP500_micro",
+    # US30U="US30",
 )
 
 
@@ -25,7 +27,7 @@ if __name__ == "__main__":
             instrument_to=instrument_to,
             write_to_csv=write_to_csv,
             do_prices=True,
-            do_multiple=False,
-            do_adjusted=False,
+            do_multiple=True,
+            do_adjusted=True,
             ignore_duplication=True,
         )
