72e7c99640d40fb6eebf3a15d73efb67c2bf7d44
Andy Geach <andy@bugorfeature.net>
2024-11-07 09:14:20 +0000

    refactor

--- systems/futures_spreadbet/dynamic_small_system_optimise/fsb_do_system.py
+++ systems/futures_spreadbet/dynamic_small_system_optimise/fsb_do_system.py
@@ -1,17 +1,7 @@
-import datetime
-
-import pandas as pd
-import yaml
-from matplotlib.pyplot import show
-
 from syscore.constants import arg_not_supplied
-from syscore.fileutils import resolve_path_and_filename_for_package
 from sysdata.config.configdata import Config
-from sysdata.sim.csv_futures_sim_data import csvFuturesSimData
-from sysdata.sim.db_futures_sim_data import dbFuturesSimData
 from syslogging.logger import get_logger
 from systems.basesystem import System
-from systems.diagoutput import systemDiag
 from systems.forecast_combine import ForecastCombine
 from systems.forecast_scale_cap import ForecastScaleCap
 from systems.forecasting import Rules
@@ -25,19 +15,27 @@ from systems.provided.dynamic_small_system_optimise.accounts_stage import (
 )
 from systems.provided.rob_system.rawdata import myFuturesRawData
 from systems.risk import Risk
+from systems.system_utils import (
+    write_pickle_file,
+    write_estimate_file,
+    write_full_config_file,
+    build_fsb_db_sim_data,
+    print_per_contract_values,
+    print_system_stats,
+    plot_system_performance,
+    plot_instrument_count_over_time,
+    plot_trading_rule_pnl,
+)
 
 # min
 # CONFIG = "systems.futures_spreadbet.config.fsb_do_minimal.yaml"
 # SAVED_SYSTEM = "systems.futures_spreadbet.pickle.fsb_do_minimal.pck"
 
-# mid
-# CONFIG = "systems.futures_spreadbet.config.fsb_static_system_mid_est.yaml"
-# SAVED_SYSTEM = "systems.futures_spreadbet.pickle.saved-do-system-mid.pck"
+CONFIG = "systems.futures_spreadbet.config.fsb_dynamic_system_v1_2.yaml"
+SAVED_SYSTEM = "systems.futures_spreadbet.pickle.fsb_dynamic_system_v1_2.pck"
 
-# max
-CONFIG = "systems.futures_spreadbet.config.fsb_dynamic_system_v1_3.yaml"
-SAVED_SYSTEM = "systems.futures_spreadbet.pickle.fsb_dynamic_system_v1_3.pck"
-# SAVED_SYSTEM = "systems.futures_spreadbet.pickle.full-small-cap.pck"
+# CONFIG = "systems.futures_spreadbet.config.fsb_dynamic_system_v1_6.yaml"
+# SAVED_SYSTEM = "systems.futures_spreadbet.pickle.fsb_dynamic_system_v1_6.pck"
 
 
 log = get_logger("backtest")
@@ -64,16 +62,16 @@ def run_do_system(
             config.use_forecast_scale_estimates = True
         system = fsb_do_system(config=config)
 
-    # print_per_contract_values(system)
-    print_system_stats(system)
-    # plot_system_performance(system)
+    # print_per_contract_values(log, system)
+    print_system_stats(log, system)
+    plot_system_performance(system)
 
     if write_pickle:
-        write_pickle_file(system)
+        write_pickle_file(log, system, SAVED_SYSTEM)
     if do_estimate:
-        write_estimate_file(system)
+        write_estimate_file(log, system, "systems.futures_spreadbet.config")
     if write_config:
-        write_full_config_file(system)
+        write_full_config_file(log, system, "systems.futures_spreadbet.config")
 
     # plot_instrument_count_over_time(system)
     # plot_trading_rule_pnl(system)
@@ -90,7 +88,6 @@ def fsb_do_system(
 
     if config is arg_not_supplied:
         config = Config("systems.futures_spreadbet.do_fsb.fsb_do_minimal.yaml")
-        config.risk_overlay = arg_not_supplied
 
     rules = Rules(trading_rules)
 
@@ -111,132 +108,6 @@ def fsb_do_system(
     )
 
     return system
-
-
-def build_fsb_csv_sim_data():
-    return csvFuturesSimData(
-        csv_data_paths=dict(
-            csvFuturesInstrumentData="fsb.csvconfig",
-            csvRollParametersData="fsb.csvconfig",
-            csvFxPricesData="data.futures.fx_prices_csv",
-            csvFuturesMultiplePricesData="fsb.multiple_prices_csv",
-            csvFuturesAdjustedPricesData="fsb.adjusted_prices_csv",
-            csvSpreadCostData="fsb.csvconfig",
-        )
-    )
-
-
-def build_fsb_db_sim_data():
-    return dbFuturesSimData(
-        csv_data_paths=dict(
-            csvFuturesInstrumentData="fsb.csvconfig",
-            csvRollParametersData="fsb.csvconfig",
-            csvSpreadCostData="fsb.csvconfig",
-        )
-    )
-
-
-def print_per_contract_values(system: System):
-    for instr in system.portfolio.get_instrument_list():
-        min_bet = system.data.get_value_of_block_price_move(instr)
-        value_per_contract = system.portfolio.get_baseccy_value_per_contract(instr)[-1]
-        per_con_val_as_proportion = (
-            system.portfolio.get_per_contract_value_as_proportion_of_capital(instr)[-1]
-        )
-        notional_pos = system.portfolio.get_notional_position(instr)[-1]
-        print(
-            f"{instr}: {min_bet=}, value_per_min_bet={value_per_contract}, "
-            f"min bet value as proportion of capital={per_con_val_as_proportion}, "
-            f"notional position={notional_pos}"
-        )
-
-
-def print_system_stats(system: System):
-    optimised = system.accounts.optimised_portfolio()
-    log.info(f"Stats: {optimised.stats()}")
-    opt_portfolio_percent = system.accounts.optimised_portfolio().percent
-    log.info(f"% Stats: {opt_portfolio_percent.stats()}")
-
-
-def plot_system_performance(system: System):
-    system.config.use_SR_costs = True
-    unrounded = system.accounts.portfolio(roundpositions=False)
-
-    system.config.use_SR_costs = False
-    rounded = system.accounts.portfolio()
-    optimised = system.accounts.optimised_portfolio()
-
-    performance = pd.concat(
-        [unrounded.curve(), rounded.curve(), optimised.curve()], axis=1
-    )
-    performance.columns = [
-        "unrounded",
-        "rounded",
-        "optimised",
-    ]
-    performance.plot(figsize=(15, 9))
-    # performance.tail(1500).plot(figsize=(15, 9))
-    # performance["1980-01-01":"1989-12-31"].plot()
-    # performance["2020-01-01":].plot()
-    show()
-
-
-def plot_instrument_count_over_time(system: System):
-    pos_df = system.optimisedPositions.get_optimised_position_df()
-    pos_df["instr_count"] = pos_df.notna().sum(axis=1)
-    pos_df["instr_count"].plot(
-        title="Instrument count (optimised portfolio)", figsize=(15, 9)
-    )
-    # pos_df["instr_count"][:"2015-01-01"].plot(title="Instrument count (optimised portfolio)", figsize=(15,9))
-    show()
-
-
-def plot_trading_rule_pnl(system: System):
-    for rule in system.rules.trading_rules():
-        pnl = system.accounts.pandl_for_trading_rule(rule)
-        pnl.plot(title=f"{rule}")
-        # pos.tail(1000).plot(title=f"{instr} (min bet {min_bet})")
-        show()
-
-
-def write_pickle_file(system):
-    log.info(f"Writing pickled system to {SAVED_SYSTEM}")
-    system.cache.pickle(SAVED_SYSTEM)
-
-
-def write_estimate_file(system):
-    now = datetime.datetime.now()
-    sysdiag = systemDiag(system)
-    output_file = resolve_path_and_filename_for_package(
-        f"systems.futures_spreadbet.config.estimate-{now.strftime('%Y-%m-%d_%H%M%S')}.yaml"
-    )
-    print(f"writing estimate params to: {output_file}")
-    estimates_needed = [
-        "instrument_div_multiplier",
-        "forecast_div_multiplier",
-        "forecast_scalars",
-        "instrument_weights",
-        "forecast_weights",
-        # "forecast_mapping",
-    ]
-
-    sysdiag.yaml_config_with_estimated_parameters(output_file, estimates_needed)
-
-
-def write_full_config_file(system):
-    now = datetime.datetime.now()
-    output_file = resolve_path_and_filename_for_package(
-        f"systems.futures_spreadbet.full_config-{now.strftime('%Y-%m-%d_%H%M%S')}.yaml"
-    )
-    print(f"writing config to: {output_file}")
-    system.config.save(output_file)
-
-
-def config_from_file(path_string):
-    path = resolve_path_and_filename_for_package(path_string)
-    with open(path) as file_to_parse:
-        config_dict = yaml.load(file_to_parse, Loader=yaml.CLoader)
-    return config_dict
 
 
 if __name__ == "__main__":
