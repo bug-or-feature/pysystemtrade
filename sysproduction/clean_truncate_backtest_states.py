32f3b53234c8491ce7ac7d9aa309651323138f8a
Andy Geach <andy@bugorfeature.net>
2025-07-22 16:04:27 +0100

    optionally compress system pickle files

--- sysproduction/clean_truncate_backtest_states.py
+++ sysproduction/clean_truncate_backtest_states.py
@@ -28,6 +28,9 @@ class cleanTruncateBacktestStates:
             directory_to_use, days_old=max_age, extension=".pck"
         )
         delete_old_files_with_extension_in_pathname(
+            directory_to_use, days_old=max_age, extension=".pckz"
+        )
+        delete_old_files_with_extension_in_pathname(
             directory_to_use, days_old=max_age, extension=".yaml"
         )
 
