e1b438262a52acd5405459e65ce9416e6c226275
Andy Geach <andy@bugorfeature.net>
2025-09-17 09:26:04 +0100

    revert ib_insync logging level to WARNING

--- syslogging/logger.py
+++ syslogging/logger.py
@@ -83,7 +83,7 @@ def _configure_sim():
     print(f"Configuring sim logging")
     handler = logging.StreamHandler(stream=sys.stdout)
     handler.setLevel(logging.DEBUG)
-    logging.getLogger("ib_insync").setLevel(logging.INFO)
+    logging.getLogger("ib_insync").setLevel(logging.WARNING)
     logging.getLogger("arctic").setLevel(logging.INFO)
     logging.getLogger("matplotlib").setLevel(logging.INFO)
     logging.basicConfig(
