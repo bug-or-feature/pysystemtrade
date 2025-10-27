fd64d593f3a85d615a0b37140ccaa70a71e1101f
Andy Geach <andy@bugorfeature.net>
2025-10-27 15:01:22 +0000

    handle shell script errors

--- sysproduction/macos/archive_mongo_dump.sh
+++ sysproduction/macos/archive_mongo_dump.sh
@@ -2,14 +2,19 @@
 
 source ~/.zprofile
 
+TRAPZERR() {
+  datetime=$(date +%Y-%m-%d\ %H\:%M\:%S)
+  echo "Problem running 'archive_mongo_dump.sh'" | /usr/bin/mail -s "FUTP Problem running 'archive_mongo_dump.sh'" $PYSYS_EMAIL
+}
+
 # date
 datetime=$(date +%Y-%m-%d)
 
 # source
-src="/Users/devuser/pst-futures/data/mongo_dump"
+src="$PYSYS_CODE/data/mongo_dump"
 
 # destination
-dest="/Users/devuser/Documents/backup"
+dest=$BACKUP_DIR
 
 # full path to compression tool
 zip=/usr/bin/zip
--- sysproduction/macos/rsync-monitor-to-site.sh
+++ sysproduction/macos/rsync-monitor-to-site.sh
@@ -1,11 +1,15 @@
 #!/bin/zsh
 
-SOURCE=/Users/devuser/Sites/pst-futures/
-TARGET=tacticalgrace::NetBackup/pst_site
+source ~/.zprofile
+
+TRAPZERR() {
+  datetime=$(date +%Y-%m-%d\ %H\:%M\:%S)
+  echo "Problem running 'rsync-monitor-to-site.sh'" | /usr/bin/mail -s "FUTP Problem running 'rsync-monitor-to-site.sh'" $PYSYS_EMAIL
+}
 
 echo "`date "+%Y-%m-%d %H:%M:%S"` starting sync of monitor site files to httpd home..."
 
-/usr/local/bin/rsync -av --exclude=.DS_Store -e "ssh -p 2222" $SOURCE $TARGET
+/usr/local/bin/rsync -av --exclude=.DS_Store -e "ssh -p 2222" $PYSYS_SITE_DIR $PYSYS_HTTPD_DIR
 
 echo "`date "+%Y-%m-%d %H:%M:%S"` sync of monitor site files done."
 
