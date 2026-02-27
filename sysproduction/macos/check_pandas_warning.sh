ddc87e3fd7e4eebd54266a1365163753c79b88ea
Andy Geach <andy@bugorfeature.net>
2026-02-27 09:43:13 +0000

    add error trap

--- sysproduction/macos/check_pandas_warning.sh
+++ sysproduction/macos/check_pandas_warning.sh
@@ -1,10 +1,14 @@
-#!/usr/bin/env zsh
+#!/bin/zsh
 
 source ~/.zprofile
 
+TRAPZERR() {
+  datetime=$(date +%Y-%m-%d\ %H\:%M\:%S)
+  echo "Problem running 'check_pandas_warning.sh'" | /usr/bin/mail -s "FUTP Problem running 'check_pandas_warning.sh'" $PYSYS_EMAIL
+}
 
 # Search for pattern and capture results
-RESULTS=$(grep -rnE 'FutureWarning|DeprecationWarning' "$ECHO_PATH" 2>/dev/null)
+RESULTS=$(grep -rnE 'FutureWarning|DeprecationWarning|ChainedAssignmentError' "$ECHO_PATH" 2>/dev/null)
 
 # Exit silently if nothing found
 if [[ -z "$RESULTS" ]]; then
@@ -16,4 +20,3 @@ SUBJECT="Future/Deprecation Warning instances found"
 BODY="$RESULTS"
 
 echo "$BODY" | /usr/bin/mail -s "$SUBJECT" "$PYSYS_EMAIL"
-
