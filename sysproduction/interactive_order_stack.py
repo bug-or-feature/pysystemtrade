87d494e65e8148cce69462d522edb9f9189d107a
Andy Geach <andy@bugorfeature.net>
2026-07-04 11:19:27 +0100

    improved strategy and broker position tables to show correct contract keys. Formatting improvements

--- sysproduction/interactive_order_stack.py
+++ sysproduction/interactive_order_stack.py
@@ -305,7 +305,7 @@ def get_futures_contract_and_qty_to_close_position(
     data: dataBlob,
 ) -> Tuple[str, str, int]:
     diag_positions = diagPositions(data)
-    contract_positions = diag_positions.get_all_current_contract_positions()
+    contract_positions = diag_positions.get_all_current_contract_positions_with_db_expiries()
     print("Current contract positions in DB")
     print(contract_positions)
 
@@ -901,24 +901,27 @@ def view_positions(data):
 
     diag_positions = diagPositions(data)
     data_optimal = dataOptimalPositions(data)
-    ans0 = data_optimal.get_pd_of_position_breaks()
-    ans1 = diag_positions.get_all_current_strategy_instrument_positions()
-    ans2 = diag_positions.get_all_current_contract_positions_with_db_expiries()
-    ans3 = data_broker.get_all_current_contract_positions()
-    print("Optimal vs actual")
-    print(ans0.sort_values("breaks"))
-    print("Strategy positions")
-    print(ans1.as_pd_df().sort_values("instrument_code"))
-    print("\n Contract level positions")
-    print(ans2.as_pd_df().sort_values(["instrument_code", "contract_date"]))
+    position_breaks = data_optimal.get_pd_of_position_breaks()
+    strategy_positions = diag_positions.get_all_current_strategy_instrument_positions()
+    db_positions = diag_positions.get_all_current_contract_positions_with_db_expiries()
+    broker_positions = data_broker.get_current_positions_with_contract_keys()
+    print("\nOptimal vs actual")
+    print(position_breaks)
+    print("\nStrategy positions")
+    print(strategy_positions.as_pd_df().sort_values("instrument_code"))
+    print("\nContract level positions")
+    print(db_positions.as_pd_df().sort_values(["instrument_code", "contract_date"]))
     breaks = diag_positions.get_list_of_breaks_between_contract_and_strategy_positions()
     if len(breaks) > 0:
         print("\nBREAKS between strategy and contract positions: %s\n" % str(breaks))
     else:
         print("(No breaks positions consistent)")
-    print("\n Broker positions")
-    print(ans3.as_pd_df().sort_values(["instrument_code", "contract_date"]))
-    breaks = data_broker.get_list_of_breaks_between_broker_and_db_contract_positions()
+    print("\nBroker positions")
+    print(broker_positions.as_pd_df().sort_values(["instrument_code", "contract_date"]))
+    breaks = data_broker.get_list_of_breaks_between_broker_and_db_contract_positions(
+        broker_positions,
+        db_positions,
+    )
     if len(breaks) > 0:
         print(
             "\nBREAKS between broker and DB stored contract positions: %s\n"
@@ -926,6 +929,7 @@ def view_positions(data):
         )
     else:
         print("(No breaks positions consistent)")
+    print("")
     return None
 
 
